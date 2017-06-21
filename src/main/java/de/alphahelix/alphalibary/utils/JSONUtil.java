a85c84a73ae65011eb8da11366cf24b8be96a279
AlphaHelixDev <max.hertel@outlook.de>
2017-06-22 01:32:18 +0200

    InventoryItemSerializer added

--- src/main/java/de/alphahelix/alphalibary/utils/JSONUtil.java
+++ src/main/java/de/alphahelix/alphalibary/utils/JSONUtil.java
@@ -283,3 +283,34 @@ class LocationDiffSerializer implements JsonSerializer<Schematic.LocationDiff>, 
         return obj;
     }
 }
+
+class InventoryItemSerializer implements JsonSerializer<InventoryItem>, JsonDeserializer<InventoryItem> {
+
+    @Override
+    public InventoryItem deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
+        JsonObject obj = (JsonObject) jsonElement;
+
+
+        return new InventoryItem() {
+            @Override
+            public ItemStack getItemStack() {
+                return JSONUtil.getGson().fromJson(obj.getAsJsonObject("item"), ItemStack.class);
+            }
+
+            @Override
+            public int getSlot() {
+                return obj.getAsJsonPrimitive("slot").getAsInt();
+            }
+        };
+    }
+
+    @Override
+    public JsonElement serialize(InventoryItem inventoryItem, Type type, JsonSerializationContext jsonSerializationContext) {
+        JsonObject obj = new JsonObject();
+
+        obj.add("item", JSONUtil.getGson().toJsonTree(inventoryItem.getItemStack()));
+        obj.addProperty("slot", inventoryItem.getSlot());
+
+        return obj;
+    }
+}

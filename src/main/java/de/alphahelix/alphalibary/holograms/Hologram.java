8ebf6a82c13aa1cc9392f9cbdaa9c0fb9163514a
BuildTools <unconfigured@null.spigotmc.org>
2017-06-27 23:35:58 +0200

    Added Holograms

---
+++ src/main/java/de/alphahelix/alphalibary/holograms/Hologram.java
@@ -0,0 +1,54 @@
+/*
+ *
+ *  * Copyright (C) <2017>  <AlphaHelixDev>
+ *  *
+ *  *       This program is free software: you can redistribute it under the
+ *  *       terms of the GNU General Public License as published by
+ *  *       the Free Software Foundation, either version 3 of the License.
+ *  *
+ *  *       This program is distributed in the hope that it will be useful,
+ *  *       but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  *       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  *       GNU General Public License for more details.
+ *  *
+ *  *       You should have received a copy of the GNU General Public License
+ *  *       along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+package de.alphahelix.alphalibary.holograms;
+
+import de.alphahelix.alphalibary.fakeapi.instances.FakeArmorstand;
+import de.alphahelix.alphalibary.fakeapi.utils.ArmorstandFakeUtil;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
+public class Hologram {
+
+	private String name;
+	private Location location;
+
+	public Hologram(String name, Location location) {
+		this.name = name; this.location = location;
+	}
+
+	public FakeArmorstand spawn(Player player) {
+		return ArmorstandFakeUtil.spawnArmorstand(player, getLocation(), getName());
+	}
+
+	public Location getLocation() {
+		return location;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public Hologram setName(String name) {
+		this.name = name; return this;
+	}
+
+	public Hologram setLocation(Location location) {
+		this.location = location; return this;
+	}
+}

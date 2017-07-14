56b1c44c60600204c7ffca81664ee04ff386f62f
AlphaHelixDev <alphahelixdev@gmail.com>
2017-07-14 04:51:18 +0200

    Added getValues method for JDB

--- src/main/java/de/alphahelix/alphalibary/mysql/JSONDatabase.java
+++ src/main/java/de/alphahelix/alphalibary/mysql/JSONDatabase.java
@@ -1,6 +1,26 @@
+/*
+ *
+ *  * Copyright (C) <2017>  <AlphaHelixDev>
+ *  *
+ *  *       This program is free software: you can redistribute it under the
+ *  *       terms of the GNU General Public License as published by
+ *  *       the Free Software Foundation, either version 3 of the License.
+ *  *
+ *  *       This program is distributed in the hope that it will be useful,
+ *  *       but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  *       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  *       GNU General Public License for more details.
+ *  *
+ *  *       You should have received a copy of the GNU General Public License
+ *  *       along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 package de.alphahelix.alphalibary.mysql;
 
 import de.alphahelix.alphalibary.utils.JSONUtil;
+
+import java.util.ArrayList;
 
 public class JSONDatabase {
 
@@ -27,6 +47,16 @@ public class JSONDatabase {
             return JSONUtil.getValue(this.database.getResult(id.name(), idValue, "val").toString(), define);
         }
         return null;
+    }
+    
+    public <T> ArrayList<T> getValues (Class<T> define) {
+        ArrayList<T> vals = new ArrayList<>();
+        
+        for(String json : this.database.getList("val")) {
+            vals.add(JSONUtil.getValue(json, define));
+        }
+        
+        return vals;
     }
 
     public boolean hasValue(String idValue) {

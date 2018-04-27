d4a6c7b8684a2dbbce9cf599a787cc5c2892c24a
AlphaHelixDev <alphahelixdev@gmail.com>
2018-04-27 20:18:19 +0200

    Removed Debug code

--- AlphaLibaryModules/FakeAPI/src/de/alphahelix/alphalibary/fakeapi/FakeModule.java
+++ AlphaLibaryModules/FakeAPI/src/de/alphahelix/alphalibary/fakeapi/FakeModule.java
@@ -2,12 +2,7 @@ package de.alphahelix.alphalibary.fakeapi;
 
 import de.alphahelix.alphalibary.core.AlphaModule;
 import de.alphahelix.alphalibary.core.Dependency;
-import de.alphahelix.alphalibary.fakeapi.gui.dimensional.DimensionalGUI;
 import org.bukkit.Bukkit;
-import org.bukkit.Material;
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.player.PlayerChatEvent;
-import org.bukkit.inventory.ItemStack;
 
 @Dependency(dependencies = {
 		"PacketListenerAPI", "StorageModule", "InventoryModule"
@@ -19,10 +14,4 @@ public class FakeModule implements AlphaModule {
 		Bukkit.getPluginManager().registerEvents(this, plugin());
 	}
 	
-	@EventHandler
-	public void onPlayerChat(PlayerChatEvent e) {
-		
-		new DimensionalGUI("test01", 0, new ItemStack(Material.GOLD_AXE), new ItemStack(Material.ANVIL)).open(e.getPlayer(), 3);
-		
-	}
 }
